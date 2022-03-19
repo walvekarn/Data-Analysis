a74a8410fab6faf9964bfd5f0bbb6010b174bafb
Nikita Walvekar <63643603+walvekarn@users.noreply.github.com>
2022-03-19 18:51:14 +0530

    Update Covid_data.sql

--- Covid_data.sql
+++ Covid_data.sql
@@ -102,3 +102,7 @@ where dea.continent is not null;
 -- Data for further visualisation.
 select *
 from Percentpopulationvaccinated
+
+
+Tableau dashboard link- 
+https://public.tableau.com/views/Covid_data_dashboard/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link
